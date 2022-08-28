d655ff1bfa84f6ae778c5786c5d99d2c0636b2fd
mohamed <mohamed.hesham.2049@gmail.com>
2022-08-28 07:08:57 +0200

    added hero section

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const hamburgerMenu = document.querySelector(".hamburger-menu");
 const navMenu = document.querySelector(".nav-links");
-
+const body = document.querySelector("body");
 hamburgerMenu.addEventListener("click", () => {
   hamburgerMenu.classList.toggle("active");
   navMenu.classList.toggle("active");
+  body.classList.toggle("scroll-hidden");
 });
 document.querySelectorAll(".nav-links li").forEach((n) =>
   n.addEventListener("click", () => {
